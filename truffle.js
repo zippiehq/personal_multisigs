99c5d932d5b651dce5b4aa88858fb4083800c291
Alex Waggoner <waggoner.alex@gmail.com>
2018-02-22 20:07:19 -0800

    added optimization to truffle config

--- truffle.js
+++ truffle.js
@@ -1,10 +1,16 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 7900000
-    }
-  }
+	networks: {
+		development: {
+			host: "localhost",
+			port: 8545,
+			network_id: "*", // Match any network id
+			gas: 7900000
+		}
+	},
+	solc: {
+		optimizer: {
+			enabled: true,
+			runs: 200
+		}
+	}
 };
