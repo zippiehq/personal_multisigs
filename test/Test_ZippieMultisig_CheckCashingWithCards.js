0461f910d0f90184f6d2aa4d814475f8660794cf
Magnus Bondesson <m4gnus@gmail.com>
2018-09-03 15:19:08 +0200

    Rename variables logically and remove lots of duplicate code in test

--- test/Test_ZippieMultisig_CheckCashingWithCards.js
+++ test/Test_ZippieMultisig_CheckCashingWithCards.js
@@ -24,44 +24,38 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 		const multisig = accounts[5] // multisig wallet (sender, don't sign with this account since the private key should be forgotten at creation)
 		const sponsor = accounts[6] // Zippie PMG server
 		
+		const addresses = [multisig, basicToken.address, recipient, verificationKey]
+		const signers = [signer, card]
 		const m = [1, 1, 1, 1]
 
-		var signByPrivateKey = await zipperMS.soliditySha3_addresses_m_cards([signer, card], m);
-		var signedByPrivateKey = web3.eth.sign(multisig, signByPrivateKey).slice(2);
-
-		var r0 = '0x' + signedByPrivateKey.slice(0,64);
-		var s0 = '0x' + signedByPrivateKey.slice(64,128);
-		var v0 = web3.toDecimal(signedByPrivateKey.slice(128,130)) + 27;
+		var multisigHash = await zipperMS.soliditySha3_addresses_m_cards(signers, m);
+		var multisigSignature = web3.eth.sign(multisig, multisigHash).slice(2);
+		const multisigSig = getRSV(multisigSignature)
 
 		// sign by multisig signer
-		var signByKey1 = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
-		var signedByKey1 = web3.eth.sign(signer, signByKey1).slice(2);
-
-		var r1 = '0x' + signedByKey1.slice(0,64);
-		var s1 = '0x' + signedByKey1.slice(64,128);
-		var v1 = web3.toDecimal(signedByKey1.slice(128,130)) + 27;
+		var blankCheckHash = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
+		var blankCheckSignature = web3.eth.sign(signer, blankCheckHash).slice(2);
+		const blankCheckSig = getRSV(blankCheckSignature)
 
 		// sign by a random verification key
-		var signByVerification = await zipperMS.soliditySha3_address(recipient);
-		var signedByVerification = web3.eth.sign(verificationKey, signByVerification).slice(2);
-
-		var r2 = '0x' + signedByVerification.slice(0,64);
-		var s2 = '0x' + signedByVerification.slice(64,128);
-		var v2 = web3.toDecimal(signedByVerification.slice(128,130)) + 27;
+		var recipientHash = await zipperMS.soliditySha3_address(recipient);
+		var recipientSignature = web3.eth.sign(verificationKey, recipientHash).slice(2);
+		const recipientSig = getRSV(recipientSignature)
 
 		const digest = '0xABCDEF'
-		var signByCard = await zipperMS.soliditySha3_sign(digest)
-		var signedByCard = web3.eth.sign(card, signByCard).slice(2);
+		var digestHash = await zipperMS.soliditySha3_sign(digest)
+		var digestSignature = web3.eth.sign(card, digestHash).slice(2);
+		const digestSig = getRSV(digestSignature)
 		
-		var rCard = '0x' + signedByCard.slice(0,64);
-		var sCard = '0x' + signedByCard.slice(64,128);
-		var vCard = web3.toDecimal(signedByCard.slice(128,130)) + 27;
+		const v = [multisigSig.v, blankCheckSig.v, digestSig.v, recipientSig.v]
+		const r = [multisigSig.r.valueOf(), blankCheckSig.r.valueOf(), digestSig.r.valueOf(), recipientSig.r.valueOf()]
+		const s = [multisigSig.s.valueOf(), blankCheckSig.s.valueOf(), digestSig.s.valueOf(), recipientSig.s.valueOf()]
 
 		var initialBalanceSender = await basicToken.balanceOf(multisig)
 		var initialBalanceRecipient = await basicToken.balanceOf(recipient)
 		assert(await zipperMS.checkCashed(multisig, verificationKey) === false, "check already marked as cashed before transfer");
 		
-		await zipperMS.checkAndTransferFrom_BlankCheck_Card([multisig, basicToken.address, recipient, verificationKey], [signer, card], m, [v0, v1, vCard, v2], [r0.valueOf(), r1.valueOf(), rCard.valueOf(), r2.valueOf()], [s0.valueOf(), s1.valueOf(), sCard.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), [signByCard], {from: sponsor});
+		await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, signers, m, v, r, s, web3.toWei(1, "ether"), [digestHash], {from: sponsor});
 
 		var newBalanceSender = await basicToken.balanceOf(multisig)
 		var newBalanceRecipient = await basicToken.balanceOf(recipient)	
@@ -71,7 +65,7 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 
 		try {
 			// try the same exact transfer
-			await zipperMS.checkAndTransferFrom_BlankCheck_Card([multisig, basicToken.address, recipient, verificationKey], [signer, card], m, [v0, v1, vCard, v2], [r0.valueOf(), r1.valueOf(), rCard.valueOf(), r2.valueOf()], [s0.valueOf(), s1.valueOf(), sCard.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), [signByCard], {from: sponsor});
+			await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, signers, m, v, r, s, web3.toWei(1, "ether"), [digestHash], {from: sponsor});
 			assert(false, "duplicate transfer went through, but should have failed!")
 		}
 		catch(error){
@@ -82,41 +76,36 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 	it("should allow a blank check to be cashed once even if no card is required, and fail the second time", async () => {
 		const signer = accounts[0] // multisig signer (1of1)
 		const recipient = accounts[2]
-		const card = accounts[3]
 		const verificationKey = accounts[4] // random verification key
 		const multisig = accounts[5] // multisig wallet (sender, don't sign with this account since the private key should be forgotten at creation)
 		const sponsor = accounts[6] // Zippie PMG server
 		
+		const addresses = [multisig, basicToken.address, recipient, verificationKey]
 		const m = [1, 1, 0, 0]
 
-		var signByPrivateKey = await zipperMS.soliditySha3_addresses_m_cards([signer], m);
-		var signedByPrivateKey = web3.eth.sign(multisig, signByPrivateKey).slice(2);
-
-		var r0 = '0x' + signedByPrivateKey.slice(0,64);
-		var s0 = '0x' + signedByPrivateKey.slice(64,128);
-		var v0 = web3.toDecimal(signedByPrivateKey.slice(128,130)) + 27;
+		var multisigHash = await zipperMS.soliditySha3_addresses_m_cards([signer], m);
+		var multisigSignature = web3.eth.sign(multisig, multisigHash).slice(2);
+		const multisigSig = getRSV(multisigSignature)
 
 		// sign by multisig signer
-		var signByKey1 = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
-		var signedByKey1 = web3.eth.sign(signer, signByKey1).slice(2);
-
-		var r1 = '0x' + signedByKey1.slice(0,64);
-		var s1 = '0x' + signedByKey1.slice(64,128);
-		var v1 = web3.toDecimal(signedByKey1.slice(128,130)) + 27;
+		var blankCheckHash = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
+		var blankCheckSignature = web3.eth.sign(signer, blankCheckHash).slice(2);
+		const blankCheckSig = getRSV(blankCheckSignature)
 
 		// sign by a random verification key
-		var signByVerification = await zipperMS.soliditySha3_address(recipient);
-		var signedByVerification = web3.eth.sign(verificationKey, signByVerification).slice(2);
+		var recipientHash = await zipperMS.soliditySha3_address(recipient);
+		var recipientSignature = web3.eth.sign(verificationKey, recipientHash).slice(2);
+		const recipientSig = getRSV(recipientSignature)
 
-		var r2 = '0x' + signedByVerification.slice(0,64);
-		var s2 = '0x' + signedByVerification.slice(64,128);
-		var v2 = web3.toDecimal(signedByVerification.slice(128,130)) + 27;
+		const v = [multisigSig.v, blankCheckSig.v, recipientSig.v]
+		const r = [multisigSig.r.valueOf(), blankCheckSig.r.valueOf(), recipientSig.r.valueOf()]
+		const s = [multisigSig.s.valueOf(), blankCheckSig.s.valueOf(), recipientSig.s.valueOf()]
 
 		var initialBalanceSender = await basicToken.balanceOf(multisig)
 		var initialBalanceRecipient = await basicToken.balanceOf(recipient)
 		assert(await zipperMS.checkCashed(multisig, verificationKey) === false, "check already marked as cashed before transfer");
 		
-		await zipperMS.checkAndTransferFrom_BlankCheck_Card([multisig, basicToken.address, recipient, verificationKey], [signer], m, [v0, v1, v2], [r0.valueOf(), r1.valueOf(), r2.valueOf()], [s0.valueOf(), s1.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), [], {from: sponsor});
+		await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, [signer], m, v, r, s, web3.toWei(1, "ether"), [], {from: sponsor});
 
 		var newBalanceSender = await basicToken.balanceOf(multisig)
 		var newBalanceRecipient = await basicToken.balanceOf(recipient)	
@@ -126,7 +115,7 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 
 		try{
 			// try the same exact transfer
-			await zipperMS.checkAndTransferFrom_BlankCheck_Card([multisig, basicToken.address, recipient, verificationKey], [signer], m, [v0, v1, v2], [r0.valueOf(), r1.valueOf(), r2.valueOf()], [s0.valueOf(), s1.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), [], {from: sponsor});
+			await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, [signer], m, v, r, s, web3.toWei(1, "ether"), [], {from: sponsor});
 			assert(false, "duplicate transfer went through, but should have failed!")
 		}
 		catch(error){
@@ -143,49 +132,39 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 		const sponsor = accounts[6] // Zippie PMG server
 		const card2 = accounts[7]
 		
+		const addresses = [multisig, basicToken.address, recipient, verificationKey]
 		const m = [1, 1, 2, 2]
 		const signers = [signer, card, card2]
 
-		var signByPrivateKey = await zipperMS.soliditySha3_addresses_m_cards(signers, m);
-		var signedByPrivateKey = web3.eth.sign(multisig, signByPrivateKey).slice(2);
-
-		var r0 = '0x' + signedByPrivateKey.slice(0,64);
-		var s0 = '0x' + signedByPrivateKey.slice(64,128);
-		var v0 = web3.toDecimal(signedByPrivateKey.slice(128,130)) + 27;
+		var multisigHash = await zipperMS.soliditySha3_addresses_m_cards(signers, m);
+		var multisigSignature = web3.eth.sign(multisig, multisigHash).slice(2);
+		const multisigSig = getRSV(multisigSignature)
 
 		// sign by multisig signer
-		var signByKey1 = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
-		var signedByKey1 = web3.eth.sign(signer, signByKey1).slice(2);
-
-		var r1 = '0x' + signedByKey1.slice(0,64);
-		var s1 = '0x' + signedByKey1.slice(64,128);
-		var v1 = web3.toDecimal(signedByKey1.slice(128,130)) + 27;
+		var blankCheckHash = await zipperMS.soliditySha3_amount_address(web3.toWei(1, "ether"), verificationKey);
+		var blankCheckSignature = web3.eth.sign(signer, blankCheckHash).slice(2);
+		const blankCheckSig = getRSV(blankCheckSignature)
 
 		// sign by a random verification key
-		var signByVerification = await zipperMS.soliditySha3_address(recipient);
-		var signedByVerification = web3.eth.sign(verificationKey, signByVerification).slice(2);
-
-		var r2 = '0x' + signedByVerification.slice(0,64);
-		var s2 = '0x' + signedByVerification.slice(64,128);
-		var v2 = web3.toDecimal(signedByVerification.slice(128,130)) + 27;
+		var recipientHash = await zipperMS.soliditySha3_address(recipient);
+		var recipientSignature = web3.eth.sign(verificationKey, recipientHash).slice(2);
+		const recipientSig = getRSV(recipientSignature)
 
 		// card 1
 		const digest = '0xABCDEF'
 		var digestHash = await zipperMS.soliditySha3_sign(digest)
-		var signedByCard = web3.eth.sign(card, digestHash).slice(2);
+		var digestSignature = web3.eth.sign(card, digestHash).slice(2);
+		const digestSig = getRSV(digestSignature)
 		
-		var rCard = '0x' + signedByCard.slice(0,64);
-		var sCard = '0x' + signedByCard.slice(64,128);
-		var vCard = web3.toDecimal(signedByCard.slice(128,130)) + 27;
-
 		// card 2
 		const digest2 = '0xFEDCBA'
 		var digestHash2 = await zipperMS.soliditySha3_sign(digest2)
-		var signedByCard2 = web3.eth.sign(card2, digestHash2).slice(2);
+		var digestSignature2 = web3.eth.sign(card2, digestHash2).slice(2);
+		const digestSig2 = getRSV(digestSignature2)
 		
-		var rCard2 = '0x' + signedByCard2.slice(0,64);
-		var sCard2 = '0x' + signedByCard2.slice(64,128);
-		var vCard2 = web3.toDecimal(signedByCard2.slice(128,130)) + 27;
+		const v = [multisigSig.v, blankCheckSig.v, digestSig.v, digestSig2.v, recipientSig.v]
+		const r = [multisigSig.r.valueOf(), blankCheckSig.r.valueOf(), digestSig.r.valueOf(), digestSig2.r.valueOf(), recipientSig.r.valueOf()]
+		const s = [multisigSig.s.valueOf(), blankCheckSig.s.valueOf(), digestSig.s.valueOf(), digestSig2.s.valueOf(), recipientSig.s.valueOf()]
 
 		const digestHashes = [digestHash, digestHash2]
 
@@ -193,10 +172,7 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 		var initialBalanceRecipient = await basicToken.balanceOf(recipient)
 		assert(await zipperMS.checkCashed(multisig, verificationKey) === false, "check already marked as cashed before transfer");
 		
-		await zipperMS.checkAndTransferFrom_BlankCheck_Card(
-				[multisig, basicToken.address, recipient, verificationKey], signers, m, [v0, v1, vCard, vCard2, v2], 
-				[r0.valueOf(), r1.valueOf(), rCard.valueOf(), rCard2.valueOf(), r2.valueOf()],
-				[s0.valueOf(), s1.valueOf(), sCard.valueOf(), sCard2.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), digestHashes, {from: sponsor});
+		await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, signers, m, v, r, s, web3.toWei(1, "ether"), digestHashes, {from: sponsor});
 
 		var newBalanceSender = await basicToken.balanceOf(multisig)
 		var newBalanceRecipient = await basicToken.balanceOf(recipient)	
@@ -208,10 +184,7 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 			// try transfer reusing card digest
 			const duplicatedDigestHashes = [digestHash, digestHash]
 
-			await zipperMS.checkAndTransferFrom_BlankCheck_Card(
-				[multisig, basicToken.address, recipient, verificationKey], signers, m, [v0, v1, vCard, vCard, v2], 
-				[r0.valueOf(), r1.valueOf(), rCard.valueOf(), rCard.valueOf(), r2.valueOf()],
-				[s0.valueOf(), s1.valueOf(), sCard.valueOf(), sCard.valueOf(), s2.valueOf()], web3.toWei(1, "ether"), duplicatedDigestHashes, {from: sponsor});
+			await zipperMS.checkAndTransferFrom_BlankCheck_Card(addresses, signers, m, v, r, s, web3.toWei(1, "ether"), duplicatedDigestHashes, {from: sponsor});
 			assert(false, "duplicated digest transfer went through, but should have failed!")
 		}
 		catch(error){
