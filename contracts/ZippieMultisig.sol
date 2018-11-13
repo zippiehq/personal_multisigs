cc2c01e6d95396c373c2132198b6b8896004c20e
Patrik Åkerlund <akerlund.patrik@gmail.com>
2018-11-13 18:02:15 +0100

    Allow card signatures to either be included or missing when amount is below limit

--- contracts/ZippieMultisig.sol
+++ contracts/ZippieMultisig.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.4.24;
 
 import "./ZippieUtils.sol";
-import "./ZippieUtils.sol";
 
 contract ZippieMultisig {
 
--- test/Test_ZippieMultisig_CheckCashingWithCards.js
+++ test/Test_ZippieMultisig_CheckCashingWithCards.js
@@ -2,7 +2,7 @@ var TestFunctions = artifacts.require("./TestFunctions.sol");
 var BasicERC20Mock = artifacts.require("./BasicERC20Mock.sol");
 var ZippieWallet = artifacts.require("./ZippieWallet.sol");
 var test;
-import { getMultisigSignature, getBlankCheckSignature, getRecipientSignature, getHardcodedDigestSignature, getSignature, getEmptyDigestSignature, getSignatureFrom3, log } from './HelpFunctions';
+import { getMultisigSignature, getBlankCheckSignature, getRecipientSignature, getHardcodedDigestSignature, getSignature, getSignatureFrom3, getEmptyDigestSignature, log } from './HelpFunctions';
 
 contract("Test Zippie Multisig Check Cashing With Cards Functionality", (accounts) => {
 
@@ -152,11 +152,10 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 		addresses = [multisig, basicToken.address, recipient, verificationKey2]
 		const blankCheckSignature = await getBlankCheckSignature(verificationKey2, signer, amountValue)
 		recipientSignature = await getRecipientSignature(recipient, verificationKey2)
-		const emptyDigestSignature = getEmptyDigestSignature()
-		const signature = getSignature(multisigSignature, blankCheckSignature, emptyDigestSignature, recipientSignature)
+		const signature = getSignatureFrom3(multisigSignature, blankCheckSignature, recipientSignature)
 		const amount = web3.utils.toWei(amountValue, "ether")
 		// Skipping card signatures, since it should work without because we are under account limit
-		await zipperWallet.redeemBlankCheck(addresses, signers, m, signature.v, signature.r, signature.s, amount, [emptyDigestSignature.digestHash], {from: sponsor});
+		await zipperWallet.redeemBlankCheck(addresses, signers, m, signature.v, signature.r, signature.s, amount, [], {from: sponsor});
 
 		var newBalanceSender = await basicToken.balanceOf(multisig)
 		var newBalanceRecipient = await basicToken.balanceOf(recipient)	
@@ -201,6 +200,44 @@ contract("Test Zippie Multisig Check Cashing With Cards Functionality", (account
 			assert(false, "Transfer went through even though card signatures are missing")
 		} catch (error) {
 			assert(error.reason == 'Invalid address found when verifying card signatures', error.reason)
+		}
+	});
+
+	it("should prevent a blank check to be cashed once from a 1 of 1 multisig with 2FA, when amount is over account limit but card signatures is missing", async () => {
+		const digestSignature = await getHardcodedDigestSignature(0, 0)
+		card = digestSignature.pubkey
+		
+		var addresses = [multisig, basicToken.address, recipient, verificationKey]
+		const signers = [signer, card]
+		const m = [1, 1, 1, 1]
+		const limitValue = "1"
+		const amountValue = "2"
+		
+		const multisigSignature = await getMultisigSignature(signers, m, multisig)
+		const limitSignature = await getBlankCheckSignature(verificationKey, signer, limitValue)
+		var recipientSignature = await getRecipientSignature(recipient, verificationKey)
+		
+		const signatureForLimit = getSignature(multisigSignature, limitSignature, digestSignature, recipientSignature)
+		assert(await zipperWallet.usedNonces(multisig, verificationKey) === false, "check already marked as cashed before transfer");
+		
+		const limit = web3.utils.toWei(limitValue, "ether")
+		await zipperWallet.setLimit(addresses, signers, m, signatureForLimit.v, signatureForLimit.r, signatureForLimit.s, limit, [digestSignature.digestHash], {from: sponsor});
+		const accountLimit = await zipperWallet.accountLimits(multisig)
+		assert(accountLimit.toString() === limit.toString(), "limit was not set correctly")
+		
+		const verificationKey2 = accounts[11] // random verification key		
+		addresses = [multisig, basicToken.address, recipient, verificationKey2]
+		const blankCheckSignature = await getBlankCheckSignature(verificationKey2, signer, amountValue)
+		recipientSignature = await getRecipientSignature(recipient, verificationKey2)
+		const signature = getSignatureFrom3(multisigSignature, blankCheckSignature, recipientSignature)
+		const amount = web3.utils.toWei(amountValue, "ether")
+		
+		try {
+			// Skipping card signatures, which should fail
+			await zipperWallet.redeemBlankCheck(addresses, signers, m, signature.v, signature.r, signature.s, amount, [], {from: sponsor});
+			assert(false, "Transfer went through even though card signatures are missing")
+		} catch (error) {
+			assert(error.reason == 'Incorrect number of signatures (v)', error.reason)
 		}
 	});
 
