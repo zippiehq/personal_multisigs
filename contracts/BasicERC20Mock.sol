952d23e4b5286476667bc775477abadc9f877247
Patrik Åkerlund <akerlund.patrik@gmail.com>
2018-11-30 10:23:00 +0100

    Implement ERC20Detailed for BasicERC20Mock token

--- contracts/BasicERC20Mock.sol
+++ contracts/BasicERC20Mock.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract BasicERC20Mock is ERC20 {
+contract BasicERC20Mock is ERC20, ERC20Detailed {
 
-    constructor(address _initialAccount) public {
+    constructor(address _initialAccount) ERC20Detailed("Zippie", "ZIPT", 18) public {
         // Set initial balance to creator
         _mint(_initialAccount, 100 ether);
     }
