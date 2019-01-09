fc39d9a6ae24beb90184de884b4322cef2e69a62
Patrik Åkerlund <akerlund.patrik@gmail.com>
2019-01-09 15:36:37 +0100

    Turn on websockets in truffle/ganache

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,8 @@ module.exports = {
 		development: {
 			host: "localhost",
 			port: 8545,
-			network_id: "*", // Match any network id
+      network_id: "*", // Match any network id
+      websockets: true 
 		}
 	},
 	solc: {
