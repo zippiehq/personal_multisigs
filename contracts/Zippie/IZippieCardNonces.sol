5a1985404aaeea1985571262cf22d17411eeb67c
Patrik Åkerlund <akerlund.patrik@gmail.com>
2019-03-26 12:51:17 +0100

    Fix: IZippieCardNonces interface didn't match the implementation (complier error in new version of Solidity for missing "view")

--- contracts/Zippie/IZippieCardNonces.sol
+++ contracts/Zippie/IZippieCardNonces.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.5.6;
  */
 interface IZippieCardNonces {
     function isNonceUsed(address signer, bytes32 nonce) 
-        external returns (bool);
+        external view returns (bool);
 
     function useNonce(address signer, bytes32 nonce, uint8 v, bytes32 r, bytes32 s) 
         external returns(bool);
