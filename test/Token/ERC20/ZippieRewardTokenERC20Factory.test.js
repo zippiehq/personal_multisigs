908fd10002f695e90ee952fc6866afe7de78e8c9
Patrik Åkerlund <akerlund.patrik@gmail.com>
2019-07-02 13:53:21 +0200

    Update OpenZeppelin Test Helpers to 0.4.0

--- test/Token/ERC20/ZippieRewardTokenERC20Factory.test.js
+++ test/Token/ERC20/ZippieRewardTokenERC20Factory.test.js
@@ -1,4 +1,5 @@
-const { BN, constants, expectEvent, shouldFail } = require("openzeppelin-test-helpers");
+const { BN, constants, expectEvent, expectRevert } = require("openzeppelin-test-helpers");
+const { expect } = require('chai');
 const ZippieRewardTokenERC20Factory = artifacts.require("ZippieRewardTokenERC20Factory");
 const { abi:rewardTokenAbi, bytecode:rewardTokenBytecode } = require('../../../build/contracts/ZippieRewardTokenERC20.json')
 
@@ -23,55 +24,32 @@ contract("ZippieRewardTokenERC20Factory", ([creator, owner]) => {
     });
 
     it("creates a token with correct details", async function () {
-      const name = await rewardToken.methods.name().call()
-      name.should.be.equal("Reward Token (XYZ)");
-
-      const symbol = await rewardToken.methods.symbol().call()
-      symbol.should.be.equal("REWARD-XYZ");
-
-      const decimals = await rewardToken.methods.decimals().call()
-      decimals.should.be.equal("6");
+      expect(await rewardToken.methods.name().call()).to.be.equal("Reward Token (XYZ)");
+      expect(await rewardToken.methods.symbol().call()).to.be.equal("REWARD-XYZ");
+      expect(await rewardToken.methods.decimals().call()).to.be.equal("6");
     });
 
     it("sets correct owner", async function () {
-      const owner = await rewardToken.methods.owner().call()
-      owner.should.be.equal(owner);
+      expect(await rewardToken.methods.owner().call()).to.be.equal(owner);
     });
 
     it("sets correct minter", async function () {
-      const creatorIsMinter = await rewardToken.methods.isMinter(creator).call()
-      creatorIsMinter.should.be.equal(false);
-
-      const ownerIsMinter = await rewardToken.methods.isMinter(owner).call()
-      ownerIsMinter.should.be.equal(true);
-
-      const factoryIsMinter = await rewardToken.methods.isMinter(this.rewardTokenFactory.address).call()
-      factoryIsMinter.should.be.equal(false);
+      expect(await rewardToken.methods.isMinter(creator).call()).to.be.equal(false);
+      expect(await rewardToken.methods.isMinter(owner).call()).to.be.equal(true);
+      expect(await rewardToken.methods.isMinter(this.rewardTokenFactory.address).call()).to.be.equal(false);
     });
 
     it("sets correct pauser", async function () {
-      const creatorIsPauser = await rewardToken.methods.isPauser(creator).call()
-      creatorIsPauser.should.be.equal(false);
-
-      const ownerIsPauser = await rewardToken.methods.isPauser(owner).call()
-      ownerIsPauser.should.be.equal(true);
-
-      const factoryIsPauser = await rewardToken.methods.isPauser(this.rewardTokenFactory.address).call()
-      factoryIsPauser.should.be.equal(false);
+      expect(await rewardToken.methods.isPauser(creator).call()).to.be.equal(false);
+      expect(await rewardToken.methods.isPauser(owner).call()).to.be.equal(true);
+      expect(await rewardToken.methods.isPauser(this.rewardTokenFactory.address).call()).to.be.equal(false);
     });
 
     it("mints reqested amount of tokens to inital owner", async function () {
-      const totalSupply = await rewardToken.methods.totalSupply().call()
-      totalSupply.should.be.equal('1000000');
-
-      const balanceCreator = await rewardToken.methods.balanceOf(creator).call()
-      balanceCreator.should.be.equal('0');
-
-      const balanceOwner = await rewardToken.methods.balanceOf(owner).call()
-      balanceOwner.should.be.equal('1000000');
-
-      const balanceFactory = await rewardToken.methods.balanceOf(this.rewardTokenFactory.address).call()
-      balanceFactory.should.be.equal('0');
+      expect(await rewardToken.methods.totalSupply().call()).to.be.equal('1000000');
+      expect(await rewardToken.methods.balanceOf(creator).call()).to.be.equal('0');
+      expect(await rewardToken.methods.balanceOf(owner).call()).to.be.equal('1000000');
+      expect(await rewardToken.methods.balanceOf(this.rewardTokenFactory.address).call()).to.be.equal('0');
     });
 
     it("uses gas", async function () {
